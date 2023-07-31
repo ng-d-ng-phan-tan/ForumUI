315b4a26a75f3cadd4fecefbe55199c504ea88fc
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2023-07-31 16:02:57 +0700

    add format distance pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './modules/auth/auth.module';
-import { QuestionsModule } from './modules/questions/questions.module';
-import { TagsModule } from './modules/tags/tags.module';
-import { UsersModule } from './modules/users/users.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
