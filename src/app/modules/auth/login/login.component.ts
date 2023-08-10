644cd2f981120f18a944f7d2747b04a20b682ccd
vuhiepp07 <vuhiep2k107@gmail.com>
2023-08-11 01:27:16 +0700

    layout component user info, add apis to auth service

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {toast} from 'src/assets/js/main.js';
 import { AuthService } from '../auth.service';
 import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -14,31 +16,35 @@ export class LoginComponent {
 
 
   constructor(private auth: AuthService,
-    private cookieService: CookieService) {}
+    private cookieService: CookieService,
+    private router: Router) {}
 
   onClickLogin(){
     // toast('Thông tin', 'Bạn vừa bấm vào nút đăng nhập','info',3000);
     // toast('Cảnh báo', 'Bạn vừa bấm vào nút đăng nhập','warning',3000);
     // toast('Thành công', 'Bạn đã đăng nhập thành công','success',3000);
     // toast('Thất bại', 'Đăng nhập thất bại','error',3000);
-  
     let obj ={
+      deviceToken: this.auth.getDeviceToken(),
       email: this.emailValue,
       password: this.passwordValue,
     }
-    this.auth.login(obj).subscribe((res) => {
+    this.auth.login(obj).subscribe((res: any) => {
       if(res.status == 200){    
         toast('Success', 'Login success','success',3000);
-        debugger
         this.cookieService.set('access_token', res.data.token, 3600);
-        this.cookieService.set('refresh_token', res.data.refreshToken, 3600)
-        console.log(this.cookieService.get('access_token'))
-        console.log(this.cookieService.get('refresh_token'))
+        this.cookieService.set('user_id', res.data.userId, 3600);
+        this.cookieService.set('refresh_token', res.data.refreshToken, 7200)
+        this.navigateToHome();
       }
       else{
         toast('Failed', res.message,'error',3000);
       }
     });
     // console.log(this.cookieService.check('access_token'))
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
   }
 }
--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { Router } from '@angular/router';
+import { AvatarsComponent } from 'src/app/modules/users/avatars/avatars.component';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from 'src/app/modules/auth/auth.service';
+
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +11,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private cookieService: CookieService,
+    private auth: AuthService
+    ) {}
+
+  userId: any;
 
   goToPage(pageName: string) {
     this.router.navigate([`${pageName}`]);
+  }
+
+  alreadyLogin(){
+    if(this.cookieService.check('access_token')){
+      this.userId = this.cookieService.get('user_id');
+      return true;
+    }
+    return false;
+  }
+
+  logOut(){
+    this.auth.logout(this.cookieService.get('access_token')).subscribe((res) => {
+      if(res.status == 200){
+        this.cookieService.delete('access_token');
+        this.cookieService.delete('refresh_token');
+      }
+    })
   }
 }
