feae9124c2f2729ad4f09f8990582250735f5f31
Buu Nguyen <74578456+maiyozkyo@users.noreply.github.com>
2023-08-11 09:43:41 +0700

    remove avatar in layout

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,9 +1,7 @@
-import { Component, ViewChild, ElementRef  } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
-import { AvatarsComponent } from 'src/app/modules/users/avatars/avatars.component';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from 'src/app/modules/auth/auth.service';
-
 
 @Component({
   selector: 'app-layout',
@@ -11,10 +9,11 @@ import { AuthService } from 'src/app/modules/auth/auth.service';
   styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent {
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     private cookieService: CookieService,
     private auth: AuthService
-    ) {}
+  ) {}
 
   userId: any;
 
@@ -22,20 +21,22 @@ export class LayoutComponent {
     this.router.navigate([`${pageName}`]);
   }
 
-  alreadyLogin(){
-    if(this.cookieService.check('access_token')){
+  alreadyLogin() {
+    if (this.cookieService.check('access_token')) {
       this.userId = this.cookieService.get('user_id');
       return true;
     }
     return false;
   }
 
-  logOut(){
-    this.auth.logout(this.cookieService.get('access_token')).subscribe((res) => {
-      if(res.status == 200){
-        this.cookieService.delete('access_token');
-        this.cookieService.delete('refresh_token');
-      }
-    })
+  logOut() {
+    this.auth
+      .logout(this.cookieService.get('access_token'))
+      .subscribe((res) => {
+        if (res.status == 200) {
+          this.cookieService.delete('access_token');
+          this.cookieService.delete('refresh_token');
+        }
+      });
   }
 }
