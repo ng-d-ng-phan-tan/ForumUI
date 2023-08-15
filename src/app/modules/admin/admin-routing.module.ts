0ef12f77b6cfda6c4feb2cea1a3cc7f87f763f16
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2023-08-15 07:51:44 +0700

    setup routing admin module

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,11 +1,53 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { LayoutComponent } from './layout/layout.component';
+import { Error404Component } from 'src/app/pages/error404/error404.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'signup',
+        component: SignupComponent,
+      },
+    ],
+  },
+  {
     path: '',
-    component: AdminComponent,
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: AdminComponent,
+      },
+      {
+        path: 'users',
+        component: UsersComponent,
+      },
+      {
+        path: 'posts',
+        component: PostsComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: Error404Component,
   },
 ];
 
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SignupComponent } from './signup/signup.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    LayoutComponent,
+    SignupComponent,
+    UsersComponent,
+    PostsComponent
+  ],
   imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
