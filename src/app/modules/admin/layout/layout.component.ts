98549cd52a4c2a8f9460eaec6f1f88be94268e18
vuhiepp07 <vuhiep2k107@gmail.com>
2023-08-20 23:38:42 +0700

    check

--- src/app/modules/admin/layout/layout.component.ts
+++ src/app/modules/admin/layout/layout.component.ts
@@ -38,6 +38,7 @@ export class LayoutComponent {
       this.userId = this.cookieService.get('user_id');
       let userInfo = localStorage.getItem('loginUser');
       if (userInfo) {
+        debugger
         this.loginUser = JSON.parse(userInfo) as User;
       }
       return true;
