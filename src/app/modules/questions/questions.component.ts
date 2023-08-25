75af592ef2baf1e88a340b2f9ac4eac560ecf90d
Nguyễn Ngọc Phú Sỹ <phusy9a11516@gmail.com>
2023-08-25 23:47:46 +0700

    add filter

--- src/app/modules/questions/questions.component.ts
+++ src/app/modules/questions/questions.component.ts
@@ -13,6 +13,7 @@ export class QuestionsComponent implements OnInit {
   curPage = '1';
   isSearching = true;
   isLoading = true;
+  isNoAnswer = false;
 
   constructor(
     private titleService: Title,
@@ -63,6 +64,43 @@ export class QuestionsComponent implements OnInit {
     this.curPage = (+this.curPage + 1).toString();
     this.questionsService
       .getQuestions(this.curPage)
+      .subscribe((result: any) => {
+        let lstUserID: any[] = [];
+        this.questions = result.data;
+        this.questions.map((question: any) => {
+          lstUserID.push(question.questioner_id);
+        });
+        let users = [];
+        this.usersService.getUsers(lstUserID).subscribe((res: any) => {
+          if (res.status === '200') {
+            users = res.data;
+
+            let result = [];
+
+            for (const question of this.questions) {
+              let user = users.filter((user: any) => {
+                return question.questioner_id === user.user_id;
+              });
+              const questionWithUser = {
+                ...question,
+                user,
+              };
+              result.push(questionWithUser);
+            }
+
+            this.questions = result;
+          }
+          this.isLoading = false;
+        });
+      });
+  }
+
+  filter() {
+    this.isLoading = true;
+    this.isNoAnswer = !this.isNoAnswer;
+    this.curPage = '1';
+    this.questionsService
+      .getQuestions(this.curPage, this.isNoAnswer)
       .subscribe((result: any) => {
         let lstUserID: any[] = [];
         this.questions = result.data;
